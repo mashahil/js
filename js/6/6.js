8a40d1d27af259f7af8de0e670fc237af7278b04
mashahil <muhemmedmashahil@gmail.com>
2017-12-26 12:38:19 +0530

    Update 6.js

--- js/6/6.js
+++ js/6/6.js
@@ -1,5 +1,5 @@
-String1=prompt("first string");
-String2=prompt("second string");
+a=prompt("first string");
+b=prompt("second string");
 c=a.concat(" "+b);//c=conc(a,b);
 document.write("String1:"+a+"<br>"+"String2:"+b+"<br>"+"After Concatenation:"+c);
 /*function conc(a,b){
